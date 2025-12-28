aee977d0032b009b1c527b71ac83641461d76e8a
smm_2271 <zalicleexu@gmail.com>
2025-12-28 17:09:53 +0000

    feat: 在home新增了heroBanner

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <div className={styles.home}>
       <PublicNav />
-      <h1>Home</h1>
+      <div className={styles.heroBanner}>
+        <p className={styles.heroBannerLittleTitle}>● 學生自主學習計畫作品</p>
+        <h1 className={styles.heroBannerTitle}>第五人格分析小工具</h1>
+        <p className={styles.heroBannerContent}>從角色勝率到BP選擇</p>
+        <p className={styles.heroBannerContent}>千場對局，逐步分析，讓你看見個人趨勢。</p>
+      </div>
     </div>
   );
 }
