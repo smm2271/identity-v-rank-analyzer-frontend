89a3402bf477f546f8b2ac379f33b2047b3718e9
smm_2271 <zalicleexu@gmail.com>
2025-12-28 17:06:55 +0000

    fix: 修正使用條款的路徑

--- src/share/public-nav/public-nav.tsx
+++ src/share/public-nav/public-nav.tsx
@@ -11,6 +11,7 @@ export default function PublicNav() {
       <div className={styles.introductionBtns}>
         <NavLink to="/features">功能介紹</NavLink>
         <NavLink to="/dashboard">儀表板展示</NavLink>
+        <NavLink to="/terms">使用條款</NavLink>
         <NavLink to="/about">關於計畫</NavLink>
       </div>
       <div className={styles.loginSignInBtns}>
