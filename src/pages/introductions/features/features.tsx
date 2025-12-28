4434d3094ffdf5b5e4d5fdbe2f8795c30c25e335
smm_2271 <zalicleexu@gmail.com>
2025-12-28 17:09:23 +0000

    feat: 新增features頁面

---
+++ src/pages/introductions/features/features.tsx
@@ -0,0 +1,10 @@
+import PublicNav from "../../../share/public-nav/public-nav";
+
+export default function Features() {
+    return (
+        <div>
+            <PublicNav />
+            <h1>功能介紹</h1>
+        </div>
+    );
+}
