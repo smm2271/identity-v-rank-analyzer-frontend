ba1b599de2da99c780643defbfa66c8c1c8ef0db
smm_2271 <zalicleexu@gmail.com>
2025-12-29 17:44:30 +0000

    feat: 添加切換顯示密碼的icon

--- src/pages/signin/login/login.tsx
+++ src/pages/signin/login/login.tsx
@@ -5,20 +5,33 @@ import { useState } from "react";
 export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [remember, setRemember] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
     return (
         <div className={styles.loginForm}>
             <h1>登入基地</h1>
             <p>歡迎回到備戰基地</p>
             <form>
                 <label htmlFor="username">電子郵件或使用者名稱</label>
-                <input type="text" placeholder="user@example.com" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <div className={styles.inputContainer}>
+                    <input type="text" placeholder="user@example.com" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <i className="fa-solid fa-user"></i>
+                </div>
 
                 <div className={styles.pwdtxt}>
                     <label htmlFor="password">密碼</label>
                     <Link to="?type=forgetPassword">忘記密碼</Link>
                 </div>
-                <input type="password" placeholder="密碼" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button>登入</button>
+                <div className={styles.inputContainer}>
+                    <input type={showPassword ? "text" : "password"} placeholder="密碼" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <i className={showPassword ? "fa-solid fa-eye" : "fa-solid fa-eye-slash"} onClick={() => setShowPassword(!showPassword)}></i>
+                </div>
+
+                <div className={styles.rememberContainer}>
+                    <input type="checkbox" name="remember" id="remember" />
+                    <label htmlFor="remember">保持登入狀態</label>
+                </div>
+                <button>立即登入</button>
             </form>
         </div>
     );
