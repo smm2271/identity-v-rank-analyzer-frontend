2542e442255ea255a0e43f51b23eb39375a35f52
smm_2271 <zalicleexu@gmail.com>
2026-02-14 19:00:19 +0000

    feat: 初始化功能介紹頁

--- src/pages/introductions/features/features.tsx
+++ src/pages/introductions/features/features.tsx
@@ -1,10 +1,16 @@
 import PublicNav from "../../../share/public-nav/public-nav";
+import styles from "./features.module.css";
 
 export default function Features() {
     return (
         <div>
             <PublicNav />
-            <h1>功能介紹</h1>
+            <div className={styles.content}>
+                <h1>功能介紹</h1>
+                <h2>
+                    建置中......
+                </h2>
+            </div>
         </div>
     );
 }
